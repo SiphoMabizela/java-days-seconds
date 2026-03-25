b4dfb3468f8d3942912ec4bd894702981deb5825
Sipho Mabizela <simabjhb025@student.wethinkcode.co.za>
2026-03-25 14:31:38 +0200

    Program completed, added scanner class to handle user input and read user input by using nextLine method, also added precedence for converting days to seconds

--- src/main/java/daysToSeconds.java
+++ src/main/java/daysToSeconds.java
@@ -4,6 +4,9 @@ public class daysToSeconds {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println();
+        System.out.println("Enter number of days:");
+        int days = Integer.valueOf(scanner.nextLine());
+        int seconds = days * 24 * 60 * 60;
+        System.out.println(seconds);
     }
 }
